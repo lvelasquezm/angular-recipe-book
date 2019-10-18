13333619265e1cec1f3cbae9aa2cfc049aa3d045
Lawrence Velasquez <law.vzmk@gmail.com>
2019-10-18 13:54:49 -0600

    Add validation for recipes withouth ingredients when fetching from Firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,29 +8,30 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Pizza',
-      'My delicious pizza :)',
-      'https://placeralplato.com/files/2015/06/pizza-Margarita.jpg',
-      [
-        new Ingredient('Cheese', 7),
-        new Ingredient('Tomatoe', 3),
-        new Ingredient('Bread', 2)
-      ]
-    ),
-    new Recipe(
-      'Hamburguer',
-      'This is a yummy hamburger!',
-      'https://www.thewholesomedish.com/wp-content/uploads/2019/04/The-Best-Classic-Hamburgers-550-500x500.jpg',
-      [
-        new Ingredient('Bread', 2),
-        new Ingredient('Meat', 1),
-        new Ingredient('Cheese', 3)
-      ]
-    )
-  ];
+  
+  private recipes: Recipe[] = [];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Pizza',
+  //     'My delicious pizza :)',
+  //     'https://placeralplato.com/files/2015/06/pizza-Margarita.jpg',
+  //     [
+  //       new Ingredient('Cheese', 7),
+  //       new Ingredient('Tomatoe', 3),
+  //       new Ingredient('Bread', 2)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Hamburguer',
+  //     'This is a yummy hamburger!',
+  //     'https://www.thewholesomedish.com/wp-content/uploads/2019/04/The-Best-Classic-Hamburgers-550-500x500.jpg',
+  //     [
+  //       new Ingredient('Bread', 2),
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Cheese', 3)
+  //     ]
+  //   )
+  // ];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
@@ -19,6 +20,14 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://angular-recipe-book-a95a6.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }))
       .subscribe(recipes => {
         console.log('GET response => ', recipes);
         this.recipesService.setRecipes(recipes);
